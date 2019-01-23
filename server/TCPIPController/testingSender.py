45291dc1a38d2b3fca99599b8f3936b58fec7bc4
Victor Chan <vchan@hit-play.com>
2019-01-23 15:02:08 -0500

    updating network discovery to include manufacturer and robustness

--- server/TCPIPController/testingSender.py
+++ server/TCPIPController/testingSender.py
@@ -1,11 +1,11 @@
 import SocketMessageSender as SMC
 import IpDeviceDiscovery as IDD
 
-print("Searching network for devices...")
+print("Searching network for devices... (Note: This may take a while for dense networks)")
 devices = IDD.find_network_devices()
 
 for index, device in enumerate(devices):
-    print("%d - IP: %s, Name: %s, MAC: %s" % (index, device[2], device[1], device[0]))
+    print("%d - IP: %s, Name: %s, MAC: %s, Manufacturer: %s" % (index, device[3], device[2], device[0], device[1]))
 device_to_connect = input("Select the number of a device to connect to:")
 sender = SMC.SocketMessageSender(ip_address=devices[int(device_to_connect)][2])
 value = 0;
