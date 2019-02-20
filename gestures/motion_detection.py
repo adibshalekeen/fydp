8b268abc134ebec420f586d911acfed89ae085ec
oliver <oliverng1602@gmail.com>
2019-02-20 15:20:23 -0500

    WIP

--- gestures/motion_detection.py
+++ gestures/motion_detection.py
@@ -7,6 +7,8 @@ import cv2
 from scipy.stats import norm
 import numpy as np
 
+downresScale = 3
+
 width = 281
 height = 500
 
@@ -28,50 +30,190 @@ rv = norm(loc = 0, scale = 1)
 x = np.arange(-4, 0, 4 / imageArray.shape[1])
 pdf = rv.pdf(x) * 2.5
 
-def process_channel(current, previous, channel):
+def threshold_frame(current, previous, threshold_value = 50):
 	# ksize = 3
 	# currentBlurred = cv2.GaussianBlur(current, (ksize, ksize), 0)
 	# previousBlurred = cv2.GaussianBlur(previous, (ksize, ksize), 0)
 	# delta = cv2.absdiff(previousBlurred, currentBlurred)
-	delta = cv2.absdiff(previous, current)
-	thresh = cv2.threshold(delta, 50, 255, cv2.THRESH_BINARY)[1]
+	delta = cv2.absdiff(current, previous)
+	threshold_value = (np.max(delta).astype(np.uint32) / 135)*threshold_value
+	print("median: ", np.median(delta), "max: ", np.max(delta), "min: ", np.min(delta))
+	image_thresh = cv2.threshold(delta, threshold_value, 255, cv2.THRESH_BINARY)[1]
 	# thresh = cv2.dilate(thresh, None, iterations=1)
 	# thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
-	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	# contours1 = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	contours = imutils.grab_contours(contours)
-	text = "Unoccupied"
+	return image_thresh
 
-	output = np.zeros((current.shape[0], current.shape[1], 3))
-	output[:,:,channel] = thresh
+def get_full_contours(current, previous, channel):
+	thresh = threshold_frame(current, previous, 75)
+	output = channel_image(thresh, channel)
+
+	im2, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	new_contours = []
+	if len(contours) > 0:
+		for i in range(0, len(contours)):
+			area = cv2.contourArea(contours[i])
+			if cv2.contourArea(contours[i]) > (750 / (2 ** (downresScale + 2))):
+				new_contours.append(contours[i])
+	x = 0
+	y = 0
+	if (len(new_contours) > 0):
+		x, y = centroid_from_contours(new_contours)
+
+	cv2.drawContours(output, new_contours, -1, (0,255,0), 1)
+	return output, (x, y)
+
+def get_approx_contours(current, previous, channel):
+	thresh = threshold_frame(current, previous)
+	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	contours = imutils.grab_contours(contours)
+
+	output = channel_image(thresh, channel)
+
+	filteredContours = []
 
 	for c in contours:
 		# if the contour is too small, ignore
-		# if cv2.contourArea(c) < args["min_area"]:
-		if cv2.contourArea(c) < 1000:
+		if cv2.contourArea(c) < 1000 / (2 ** (downresScale + 2)):
 			continue
-		
 		# compute the bounding box for the contour, draw it on the frame
 		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
+		filteredContours.append(cv2.boundingRect(c))
+		# cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	cv2.putText(output, "Room Status: {}".format(text), (10, 20), 
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(output, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-		(10, output.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	return output, filteredContours
 
+def relative_position(frameShape, contours):
+	f_h = frameShape[0]
+	f_w = frameShape[1]
+	i = 0
+	while i < len(contours): 
+		# (x, y, w, h)
+		c = contours[i]
+		contours[i] = (c[0]/f_w, c[1]/f_h, c[2]/f_w, c[3]/f_h)
+		i = i + 1
+	return contours
+
+def centroid_from_contours(contours):
+	x_sum = 0
+	y_sum = 0
+	count = 0
+	for c in contours:
+		count += c.shape[0]
+		sum = np.sum(c, axis=0)
+		x_sum += sum[0, 0]
+		y_sum += sum[0, 1]
+	return int(x_sum / count), int(y_sum / count)
+		
+def channel_image(frame, channel):
+	output = np.zeros((frame.shape[0], frame.shape[1], 3))
+	output[:,:,channel] = frame
 	return output
 
-def process_image(frame, previousFrame):
-	red = process_channel(frame[:,:,0], previousFrame[:,:,0], 0)
-	green = process_channel(frame[:,:,1],previousFrame[:,:,1], 1)
-	blue = process_channel(frame[:,:,2],previousFrame[:,:,2], 2)
+def process_approx_image_contours(frame, previousFrame):
+	red,r_c = get_approx_contours(frame[:, :, 0], previousFrame[:, :, 0], 0)
+	green,g_c = get_approx_contours(frame[:, :, 1], previousFrame[:, :, 1], 1)
+	blue,b_c = get_approx_contours(frame[:, :, 2], previousFrame[:, :, 2], 2)
+	r_c = relative_position(frame.shape, r_c)
+	g_c = relative_position(frame.shape, g_c)
+	b_c = relative_position(frame.shape, b_c)
+	return red,r_c,green,g_c,blue,b_c
+
+def add_frame_centroid(centroid, frame, color=(0,255,0)):
+	x = int(centroid[0])
+	y = int(centroid[1])
+	if x > 0 and y > 0: 
+		cv2.rectangle(frame, (x, y), (x + 10, y + 10), color, 1)
+	return frame
+
+def add_frame_path_centroid(centroids, frame, color=(0,255,0)):
+
+
+def process_image_contours(frame, previousFrame):
+	red, r_c = get_full_contours(frame[:, :, 0], previousFrame[:, :, 0], 0)
+	green, g_c = get_full_contours(frame[:, :, 1], previousFrame[:, :, 1], 1)
+	blue, b_c = get_full_contours(frame[:, :, 2], previousFrame[:, :, 2], 2)
+
+	red = add_frame_centroid(r_c, red)
+	green = add_frame_centroid(g_c, green)
+	blue = add_frame_centroid(b_c, blue)
+	return red, r_c, green, g_c, blue, b_c
+
+
+def centroid(thresh):
+	moment = cv2.moments(thresh)
+	if moment["m00"] > 0:
+		cx = int(moment["m10"]/moment["m00"])
+		cy = int(moment["m01"]/moment["m00"])
+		return cx, cy
+	return None
+
+def process_image_threshold(frame, previousFrame):
+	red = threshold_frame(frame[:,:,0], previousFrame[:,:,0])
+	green = threshold_frame(frame[:,:,1],previousFrame[:,:,1])
+	blue = threshold_frame(frame[:,:,2],previousFrame[:,:,2])
+
+	r_c = centroid(red)
+	g_c = centroid(green)
+	b_c = centroid(blue)
+
+	red = channel_image(red, 0)
+	if r_c is not None:
+		x = int(r_c[0])
+		y = int(r_c[1])
+		cv2.rectangle(red, (x, y), (x + 5, y + 5), (0, 255, 0), 1)
+	green = channel_image(green, 1)
+	blue = channel_image(blue, 2)
 	return red, green, blue
+
+def downResImage(frame, i): 
+	if i == 0:
+		return frame
+	else:
+		return cv2.pyrDown(downResImage(frame, i-1))
+
+def make_visual_output(frame, red, green, blue):
+	frame = imutils.resize(frame, width=500)
+	red = imutils.resize(red, width=500)
+	green = imutils.resize(green, width=500)
+	blue = imutils.resize(blue, width=500)
+	out_h = red.shape[1]
+	out_w = red.shape[0]
+	out = np.zeros((out_w * 2, out_h * 2, 3))
+	out[0:out_w, 0:out_h] = frame
+	out[0:out_w, out_h:2*out_h, :] = red
+	out[out_w:2*out_w, 0:out_h, :] = green
+	out[out_w:2*out_w, out_h:2*out_h, :] = blue
+	return out
+
+def average_centroid(*args):
+	if len(args) == 0:
+		return 0, 0
+	sum_x = 0
+	sum_y = 0
+	count = 0
+	for c in args:
+		if c[0] == 0 and c[1] == 0:
+			continue
+		sum_x += c[0]
+		sum_y += c[1]
+		count += 1
+	if count == 0: 
+		return 0, 0
+	return int(sum_x / count), int(sum_y / count)
+
+def add_centroid(centroids, centroid):
+	centroids.append(centroid)
+
+onlyOnce = True
+
+frames = []
+all_centroids = []
 
 while True:
 	stime = time.time()
-	frame = vs.read()
+	fulres = vs.read()
+	frame = fulres
+	frame = downResImage(fulres, downresScale)
 	frame = frame if args.get("video", None) is None else frame[1]
 	text = "Unoccupied"
 
@@ -79,28 +221,45 @@ while True:
 	if frame is None: 
 		break
 
-	# resize the frame, convert it to grayscale and blur it
-	frame = imutils.resize(frame, width=500)
+	height = frame.shape[1]
+	width = frame.shape[0]
+
+	# frame = imutils.resize(frame, width=500)
 
 	# if the first frame is None, initialize it
 	if firstFrame is None:
 		firstFrame = frame
 		continue
 
-	red, green, blue = process_image(frame, firstFrame)
+	# red,r_c,green,g_c,blue,b_c = process_approx_image_contours(frame, firstFrame)
+	# red, green, blue = process_image_threshold(frame, firstFrame)
+	red,r_c,green,g_c,blue,b_c = process_image_contours(frame, firstFrame)
 
 	# show the frame and record if the user presses a key
-	output = np.zeros((width * 2, height * 2, 3))
-	output[0:width, 0:height] = frame
-	output[0:width, height:2*height, :] = red
-	output[width:2*width, 0:height, :] = green
-	output[width:2*width, height:2*height, :] = blue
+	frame_output = frame.copy()
+	add_centroid(all_centroids, average_centroid(r_c, g_c, b_c))
+	frame_output = add_frame_centroid(a_centroid, frame_output, (255,255,255))
+	output = make_visual_output(frame_output, red, green, blue)
 	cv2.imshow("output", output.astype(np.uint8))
+
+	if (onlyOnce):
+		# if (len(r_c) > 5):
+		# 	print("image written to file")
+		# 	cv2.imwrite('sample.jpg', imutils.resize(red, width=500))
+		onlyOnce = False
 
 	# if the `q` key is pressed, break from the loop
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
 		break
+
+	if (len(frames) < 15):
+		frames.append(frame)
+	else:
+		del frames[0]
+		frames.append(frame)
+	
+	# firstFrame = np.average(frames, axis=0).astype(dtype=np.uint8)
 
 	# imageArray = np.roll(imageArray, -1, axis=1)
 	# imageArray[:, imageArray.shape[1] - 1] = gray.reshape(width * height)	
