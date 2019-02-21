f98e011d8ba7c170564425e82a91f85f531bf535
Victor Chan <vchan@hit-play.com>
2019-02-20 20:26:14 -0500

    Updating web interface

--- server/User_Interface/dist/IpDeviceDiscovery.py
+++ server/User_Interface/dist/IpDeviceDiscovery.py
@@ -3,7 +3,11 @@ import re
 import os
 
 
+# Function to find the IP connected devices on the network
 def find_network_devices(debug=False):
+
+    # Running local device ip settings command on linux is 'ifconfig' and
+    # on windows is 'ipconfig'
     if os.name == "nt":
         ipconfigResp = subprocess.run(['ipconfig'], stdout=subprocess.PIPE)
         ipconfigResp = ipconfigResp.stdout.decode('utf-8')
@@ -15,16 +19,21 @@ def find_network_devices(debug=False):
         userIpAddress = re.findall('inet\s(\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', ipconfigResp)
         userIpAddress = userIpAddress[0] + "/24"
 
+    # nmap command on linux requires superuser
     processToRun = ['sudo', 'nmap', '-sn', userIpAddress] if os.name == "posix" else ['nmap', '-sn', userIpAddress]
 
     if debug:
         print("Running command \'%s\'" % processToRun)
 
     response = subprocess.run(processToRun, stdout=subprocess.PIPE)
+
+    # Returned console output from the nmap command
     devices = response.stdout.decode('utf-8')
 
     if debug:
         for ip in ip_addresses:
             print(ip)
+
+    # Regular expression to capture IP address, MAC address, Device name and Manufacturer
     ip_addresses = re.findall('Nmap.*for\s([a-zA-Z0-9.\-]*)\s.*(\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*\n.*\n.*?:\s(.*?)\s\(([a-zA-Z0-9\(\)\s]*)\)', devices)
     return ip_addresses
--- server/User_Interface/dist/MappingInterfaceCtrl.py
+++ server/User_Interface/dist/MappingInterfaceCtrl.py
@@ -4,18 +4,16 @@ import sys
 
 
 class MappingInterfaceCtrl:
-    def __init__(self, filename = "routing.csv"):
+    def __init__(self, filename = "devices.csv"):
         self.filename = filename
         if not os.path.isfile(self.filename):
             with open(self.filename, 'w', newline='') as csvfile:
                 filewriter = csv.writer(csvfile, delimiter=',',
                                         quoting=csv.QUOTE_MINIMAL)
-                if "routing" in filename:
-                    filewriter.writerow(["Source_Ip_Address", "Source_Mac_Address",
-                    "Dest_Ip_Address", "Dest_Mac_Address"])
+                if "devices" in filename:
+                    filewriter.writerow(["Friendly_Name", "Ip_Address", "Mac_Address"])
                 elif "action" in filename:
-                    filewriter.writerow(["Source_Ip_Address", "Source_Action",
-                    "Dest_Ip_Address", "Dest_Action"])
+                    filewriter.writerow(["Source_Name", "Source_Action", "Dest_Name", "Dest_Action"])
 
     # def add_entry(self, routing_entry):
     #     with open(self.filename, 'a', newline='') as csvfile:
@@ -42,16 +40,23 @@ class MappingInterfaceCtrl:
     #     file_contents.pop(int(index))
     #     self.write_to_file(file_contents)
 
-    # Can optimize this to use following command to check last edit time for file
-    # file_epoch_time = os.path.getmtime(file)
-    # time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_epoch_time))
-    # def get_mapped_addresses(self, source_ip):
-    #     mapped_addresses = []
-    #     contents = self.get_file_contents()
-    #     for mapping in contents:
-    #         if source_ip in mapping:
-    #             mapped_addresses.append(mapping[2])
-    #     return mapped_addresses
+    def get_mapped_addresses(self, source_name, action = None):
+        mapped_addresses = []
+        contents = self.get_file_contents()
+        if action:
+            for mapping in contents:
+                if source_name == mapping[0]:
+                    if action == mapping[1]:
+                        msg = mapping[2] + "|" + mapping[3]
+                        mapped_addresses.append(msg)
+        else:
+            for mapping in contents:
+                for source in source_name:
+                    old_src, action = source.split("|")
+                    if old_src in mapping and not old_src == '.' :
+                        map = mapping[1] + "|" + action
+                        mapped_addresses.append(map)
+        return mapped_addresses
 
     def get_file_contents(self):
         file_contents = []
@@ -71,7 +76,7 @@ def main():
     if( len(sys.argv) != 1):
         # The node server may confuse its server.js path with this module so we specify
         function_code = sys.argv[1]
-        fileName = "./dist/routing.csv" if ("map" in sys.argv[2]) else "./dist/action.csv"
+        fileName = "./dist/devices.csv" if ("map" in sys.argv[2]) else "./dist/action.csv"
         mapCont = MappingInterfaceCtrl(fileName)
 
         if function_code == 'save_mappings':
@@ -79,13 +84,21 @@ def main():
             contents = sys.argv[3]
             rows = contents.split('|')
             rows.pop()
-            item = [["Source_Ip_Address", "Source_Mac_Address", "Dest_Ip_Address", "Dest_Mac_Address"]]
 
-            for row in rows:
-                s_ip, s_mac, d_ip, d_mac = row.split(',')
-                item.append([s_ip, s_mac, d_ip, d_mac])
+            if "devices" in fileName:
+                item = [["Friendly_Name", "Ip_Address", "Mac_Address"]]
+                for row in rows:
+                    name, ip, mac = row.split(',')
+                    item.append([name, ip, mac])
 
-            mappings = mapCont.write_to_file(item)
+                    mapCont.write_to_file(item)
+            elif "action" in fileName:
+                item = [["Source_Name", "Source_Action", "Dest_Name", "Dest_Action"]]
+                for row in rows:
+                    s_name, s_act, d_name, d_act = row.split(',')
+                    item.append([s_name, s_act, d_name, d_act])
+
+                    mapCont.write_to_file(item)
         elif function_code == 'get_mappings':
             mappings = mapCont.get_file_contents()
 
@@ -95,4 +108,14 @@ def main():
             for line in mappings:
                 for item in line:
                     print(item)
+        elif function_code == 'send_message':
+            mapDevices = MappingInterfaceCtrl("./dist/devices.csv")
+            # We defaulted to create the mapping reader, must make an action reader here
+            contents = sys.argv[3]
+            s_name, s_act = contents.split("|")
+            dest_addrs = mapCont.get_mapped_addresses(s_name, s_act)
+            resp = mapDevices.get_mapped_addresses(dest_addrs)
+            for line in resp:
+                print(line)
+
 main()
